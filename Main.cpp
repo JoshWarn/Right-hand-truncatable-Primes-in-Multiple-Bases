c1b0a35f95a7fb41a8446431181db7ef78ee3993
lmbellis <70070682+lmbellis@users.noreply.github.com>
2021-02-18 17:32:19 -0600

    Complete Rewrite
    Still in an "in-progress" state.
    Splits up bases into "subbases" - being a one-digit-long prime of the base. This allows for the multithreading to be more asynchronous - permitting a higher efficiency. Furthermore, this decreases the task size allowing for progress to be saved more often (though this still needs work).

--- Main.cpp
+++ Main.cpp
@@ -1,77 +1,60 @@
-#include <iostream> 
-#include <thread>
-#include<array>
-#include"threaded.h"
-#include<string>
+#include"master_threading.h"
+#include"file_management.h"
+#include"math_functions.h"
 #include<vector>
-#include<fstream>
-#include <filesystem>
+#include<thread>
+#include<iostream>
 
-typedef unsigned long long int ulli;
-typedef unsigned int ui;
+int main()
+{
+	int const thread_count = 12;												// thread count (1, inf)
+	int base_start = 75;															// starting base (2, )? Maybe start with 3.
+	int base_end = 75;															// ending base
+	int base = base_start;
+	std::string path("C:/Users/source/repos/RHTP_6/RHTP_6");	// working directory
+	std::string sb_ext(".subbase");												// extension for subbase files
+	std::string b_ext(".csv");													// extension for base files
+	std::string b_filename("basedata");											// Name of combined base data file
 
-int main() {
 
-	using namespace std;
-	static const int thread_count = 12;										// Number of threads to use
-	thread t[thread_count];
-	ui const end_base = 1000;									// Number of bases to calculate RHT primes. Starts at 3.
-	int start_base = 3;										// Default starting base - can be overwritten if base is detected in existing data file
-	std::vector<string> string_cache(thread_count);
-	string string_matrix[thread_count];
+	std::thread t[thread_count];
+	// starting base number 
+	int file_base = base_reader(b_filename, b_ext);
 
-	auto start = std::chrono::high_resolution_clock::now();		// starting timer
+	// TODO: Add code that removes and empty subfiles. Allows for proper continuation of a base.
 
-	if (std::filesystem::exists("data.csv")) {
-		fstream data("data.csv", ios::in);
-		string line;
-		std::getline(data, line);				// Throws away header
-		while (getline(data, line)) {
-			std::cout << line << endl;
-			std::string::size_type pos = line.find(', ');	// finds location of first ', '
-
-			if (pos != std::string::npos) {
-				start_base = std::stoi(line.substr(0, pos)) + 1;
-			}
-		}
-		data.close();
-		std::cout << "Starting Base = " << start_base << endl;
-	}
-	else {
-		fstream data("data.csv", ios::app);
-		data << "base, prime count, digit count, digit sum, largest prime" << endl;
-		data.close();
-		std::cout << "File DNE or Empty: Starting_base = 3" << endl;
+	if (file_base > base_start)
+	{
+		base = file_base;
 	}
 
-	int base = start_base;
+	// Main Loop:
+	for (base ; base <= base_end; base++)
+	{
+		std::cout << "Base = " << base << std::endl;
+		//Generate list of all primes less than base
+		std::vector<int> subprimes = eratosthene(base);
+		//List of all primes less than half base
+		std::vector<int> modprimes = modulusprimes(subprimes, base);
 
-	while (base < end_base) {
-		cout << "Base Chunk: "<< base << " to " << base + thread_count << endl;
-		for (int thread = 0; thread < thread_count; thread++) {
-			int tested_base = base + thread;
-			t[thread] = std::thread(threading, tested_base);
+		// Opening threads
+		for (int thread = 0; thread < thread_count; thread++)
+		{
+			//std::cout << "thread " << thread + 1 << std::endl;
+			t[thread] = std::thread(master_threading, subprimes, modprimes, base, path, sb_ext);
 		}
 
-		for (int thread = 0; thread < thread_count; thread++) {
+		// Closing threads
+		for (int thread = 0; thread < thread_count; thread++)
+		{
 			t[thread].join();
 		}
-		base += thread_count;
 
-		fstream data("data.csv", ios::app);
-		for (int thread = 0; thread < thread_count; thread++) {
-			fstream cache(to_string(thread) + ".csv", ios::in);
-			string line;
-			std::getline(cache, line);
-			std::cout << line << endl;
-			data << line << endl;
-		}
-		data.close();
+
+		// Combining the data from each subfile into a major file
+		base_writer(base, path, sb_ext, b_filename, b_ext);
+		// removing all subfilies
+
 	}
-
-	auto end = std::chrono::high_resolution_clock::now();
-	auto elapse = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-	std::cout << "Total Time Elapsed: " << elapse << endl;
-
+	std::cout << "Task Completed" << std::endl;
 }
-
