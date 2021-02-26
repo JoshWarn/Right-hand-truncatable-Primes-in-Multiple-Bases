007f2f6c2c156a4561ded1a2ffa5230337e9c714
JoshWarn <70070682+JoshWarn@users.noreply.github.com>
2021-02-26 16:20:10 -0600

    Added subbase_cleaner

--- file_management.h
+++ file_management.h
@@ -5,4 +5,4 @@ int subbase_selector(std::vector<int> base_primes, int base, std::string path, s
 void subbase_writer(int subbase, std::string extension, uint64_t identifier, int primecount, int digitcount, int digitsum, std::vector<int> distribution);
 void base_writer(int base, std::string path, std::string ext, std::string b_filename, std::string b_ext);
 int base_reader(std::string filename, std::string extension);
-
+void subbase_cleaner(std::string path, std::string sb_ext);
